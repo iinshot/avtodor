f420c1f8cf2cede545778a7a9668e8328b66ca86
iinshot <ernest_s@bk.ru>
2025-12-23 05:38:13 +0300

    Класс для работы со стратегией парсинга файлов

--- app/services/file_import.py
+++ app/services/file_import.py
@@ -1,9 +1,10 @@
-from strategy_parser.pdf_strategy import PdfStrategy
-from strategy_parser.csv_strategy import CsvStrategy
-from strategy_parser.xlsx_strategy import XlsxStrategy
-from strategy_parser.base_strategy import BaseStrategy
+from ..services.strategy_parser.pdf_strategy import PdfStrategy
+from ..services.strategy_parser.csv_strategy import CsvStrategy
+from ..services.strategy_parser.xlsx_strategy import XlsxStrategy
+from ..services.strategy_parser.base_strategy import BaseStrategy
 from ..services.avtodor_db import AvtodorDB
 from ..services.normalize_files import normalize_dataframe
+from ..services.progress_tracker import progress_tracker
 
 class FileImport:
 
@@ -15,11 +16,22 @@ class FileImport:
 
     @classmethod
     async def import_file(cls, ext: str, file):
+        await progress_tracker.set(10)
         parser: BaseStrategy = cls.PARSERS.get(ext)
+        await progress_tracker.set(20)
         if not parser:
             raise ValueError(f"Неподдерживаемый тип файла: {ext}")
-
+        await progress_tracker.set(30)
+        await progress_tracker.set(40)
+        await progress_tracker.set(50)
         df = parser.parse(file)
+        await progress_tracker.set_items(len(df))
+        await progress_tracker.set(60)
+        await progress_tracker.set(70)
         df_norm = normalize_dataframe(df)
+        await progress_tracker.set_items(len(df_norm))
+        await progress_tracker.set(80)
+        await progress_tracker.set(90)
         saved = await AvtodorDB.bulk_create_transactions(df_norm)
+        await progress_tracker.set(100)
         return saved, len(df_norm)
